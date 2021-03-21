b736ac5774baa8db2bffa66b23e81a5bc060e146
praveen <praveen.kumar02@hotmail.com>
2021-03-21 10:17:17 +0530

    47 , 48 create account management microservice

--- src/main/java/com/scratchdeveloper/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
+++ src/main/java/com/scratchdeveloper/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
@@ -2,8 +2,10 @@ package com.scratchdeveloper.photoapp.api.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoAppApiAccountManagementApplication {
 
 	public static void main(String[] args) {
